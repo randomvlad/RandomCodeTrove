b2f0d27ad839cab756b842c8e693cebbb5219b45
Vlad Poskatcheev <vposkatcheev@gmail.com>
2016-01-21 13:14:25 -0800

    Sentence Permutation - work in progress

--- src/main/java/string/sentence/SentencePermuteMain.java
+++ src/main/java/string/sentence/SentencePermuteMain.java
@@ -1,5 +1,13 @@
 package string.sentence;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 /**
  * @see SentencePermuteTest
  */
@@ -7,6 +15,48 @@ public class SentencePermuteMain {
 
 	public static void main( String[] args ) {
 
+		Scanner scanner = new Scanner( System.in );
+		String input = scanner.nextLine();
+		scanner.close();
+
+		List<List<String>> groups = extractGroups( input );
+	
+		permutations( new ArrayList<>(), 0, groups );
+	}
+	
+	public static void permutations( List<String> permutation, int groupIndex, List<List<String>> groups ) {
+		if ( groupIndex == groups.size() ) {
+			System.out.println( permutation );
+			return;
+		}
+
+		for ( String element : groups.get( groupIndex ) ) {
+
+			// not a fan of constantly re-creating lists
+			// could do a String[ group1.size() * group2.size() etc ... ][ number groups ]
+			// Example: (A,B) + (1,2) + (x,y,z) => String[ 12 ][ 3 ]; Keep track of permutation ID somehow
+			List<String> newPermute = new ArrayList<>( permutation );
+			newPermute.add( element );
+
+			permutations( newPermute, groupIndex + 1, groups );
+		}
+	}
+
+	public static List<List<String>> extractGroups( String value ) {
+
+		List<List<String>> groups = new ArrayList<>();
+
+		Matcher matcher = Pattern.compile( "\\{([^\\}]+)" ).matcher( value );
+
+		while ( matcher.find() ) {
+			groups.add( 
+					Arrays.stream( matcher.group( 1 ).split( "," ) )
+					.map( s -> s.trim() )
+					.collect( Collectors.toList() ) 
+			);
+		}
+
+		return groups;
 	}
 
 }
